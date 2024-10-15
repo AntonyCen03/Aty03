c6d8d1e351382586399564ec4c8f6ca375d8bc83
Antony Cen <antony.cen@correo.unimet.edu.ve>
2024-10-14 23:53:14 -0400

    versión 1.2
    versión 1.2

--- Registro_de_Vehiculo.py
+++ Registro_de_Vehiculo.py
@@ -5,10 +5,6 @@ class Vehiculo:
         self.color=color
         self.años=años
         self.tipo_vehiculo=tipo_vehiculo
-
-class Carro(Vehiculo):
-    def __init__(self, marca, modelo,color,años,tipo_vehiculo):
-        super().__init__(marca, modelo,color,años,tipo_vehiculo)
     def __str__(self) -> str:
         return(f"""
 Tipo de Vehiculo: {self.tipo_vehiculo}
@@ -17,32 +13,34 @@ Modelo: {self.modelo}
 Color: {self.color}
 Años: {self.años}
 """)
+
+class Carro(Vehiculo):
+    def __init__(self, marca, modelo,color,años,tipo_vehiculo):
+        super().__init__(marca, modelo,color,años,tipo_vehiculo)
+    def __str__(self) -> str:
+        base_str = super().__str__()
+        return base_str
 
 class Barco(Vehiculo):
     def __init__(self, marca, modelo,color,años,tipo_vehiculo):
         super().__init__(marca, modelo,color,años,tipo_vehiculo)
     
     def __str__(self) -> str:
-        return(f"""
-Tipo de Vehiculo: {self.tipo_vehiculo}
-Marca: {self.marca}
-Modelo: {self.modelo} 
-Color: {self.color}
-Años: {self.años}
-""")
+        base_str = super().__str__()
+        return base_str
 
 class Avion(Vehiculo):
     def __init__(self,marca, modelo,color,años,tipo_vehiculo):
         super().__init__(marca, modelo,color,años,tipo_vehiculo)
 
     def __str__(self) -> str:
-        return(f"""
-Tipo de Vehiculo: {self.tipo_vehiculo}
-Marca: {self.marca}
-Modelo: {self.modelo} 
-Color: {self.color}
-Años: {self.años}
-""")
+        base_str = super().__str__()
+        return base_str
+
+
+def registrar_vehiculo(vehiculo):
+    # Función para registrar un vehículo
+    print(f"Registrando vehículo: {vehiculo.marca} {vehiculo.modelo} {vehiculo.color} {vehiculo.años} {vehiculo.tipo_vehiculo}")
     
 def main():
     carro1=Carro("Toyota","corolla","rojo","2020","Carro")
@@ -57,39 +55,30 @@ def main():
     print(avion1)
     avion2=Avion("Boeing","747","Blanco","2018","Avion")
     print(avion2)
-
-if __name__=="__main__":
-    main()
-
-# Función para registrar un vehículo
-def registrar_vehiculo(vehiculo):
-    print(f"Registrando vehículo: {vehiculo.marca} {vehiculo.modelo} {vehiculo.color} {vehiculo.años} {vehiculo.tipo_vehiculo}")
-
-# Menú de opciones
-def mostrar_menu():
     print("Seleccione el tipo de vehículo que desea registrar:")
     print("1. Carro")
     print("2. Barco")
     print("3. Avión")
     print("Marca 'x' para salir ")
+    while True:
+        opcion = input("Ingrese el número de opción: ")
 
-mostrar_menu()
-while True:
-    opcion = input("Ingrese el número de opción: ")
+        if opcion=="x":
+            break
+        if opcion == "1":
+            carro = Carro(input("Marca: "),input("Modelo: "),input("Color: "),input("Años: "),input("Tipo de Vehiculo: "))
+            registrar_vehiculo(carro)
+            print(carro)
+        elif opcion == "2":
+            barco = Barco(input("Marca: "),input("Modelo: "),input("Color: "),input("Años: "),input("Tipo de Vehiculo: "))
+            registrar_vehiculo(barco)
+            print(barco)
+        elif opcion == "3":
+            avion = Avion(input("Marca: "),input("Modelo: "),input("Color: "),input("Años: "),input("Tipo de Vehiculo: "))
+            registrar_vehiculo(avion)
+            print(avion)
+        else:
+            print("Opción inválida.")
 
-    if opcion=="x":
-        break
-    if opcion == "1":
-        carro = Carro(input("Marca: "),input("Modelo: "),input("Color: "),input("Años: "),input("Tipo de Vehiculo: "))
-        registrar_vehiculo(carro)
-        print(carro)
-    elif opcion == "2":
-        barco = Barco(input("Marca: "),input("Modelo: "),input("Color: "),input("Años: "),input("Tipo de Vehiculo: "))
-        registrar_vehiculo(barco)
-        print(barco)
-    elif opcion == "3":
-        avion = Avion(input("Marca: "),input("Modelo: "),input("Color: "),input("Años: "),input("Tipo de Vehiculo: "))
-        registrar_vehiculo(avion)
-        print(avion)
-    else:
-        print("Opción inválida.")
+if __name__=="__main__":
+    main()
